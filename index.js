d93d82199b8114f5a6a1ec453278c88658e4c87f
Charlie <charlie.mahoneyy@gmail.com>
2022-11-07 21:13:59 -0500

    Start new home page

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
-const MENU_BUTTON = document.querySelector('.hero__menu-btn');
-const NAV = document.querySelector('.hero__nav');
-const navLink = document.querySelectorAll('.hero__nav-link');
-const OVERLAY = document.querySelector('.overlay');
-const HEADER = document.querySelector('.hero');
-const SEAL = document.querySelector('.seal');
+const MENU_BUTTON = document.querySelector(".hero__menu-btn");
+const NAV = document.querySelector(".hero__nav");
+const navLink = document.querySelectorAll(".hero__nav-link");
+const OVERLAY = document.querySelector(".overlay");
+const HEADER = document.querySelector(".hero");
+const SEAL = document.querySelector(".seal");
 
 // NAVIGATION
 
 let navOpen = false;
 
-MENU_BUTTON.addEventListener('click', function () {
-  MENU_BUTTON.classList.toggle('hidden');
-  NAV.classList.toggle('opened');
-  OVERLAY.classList.toggle('hidden');
+MENU_BUTTON.addEventListener("click", function () {
+  MENU_BUTTON.classList.toggle("hidden");
+  NAV.classList.toggle("opened");
+  OVERLAY.classList.toggle("hidden");
   navOpen = !navOpen;
 });
 
 function closeNav() {
-  NAV.classList.remove('opened');
-  MENU_BUTTON.classList.remove('hidden');
-  OVERLAY.classList.add('hidden');
+  NAV.classList.remove("opened");
+  MENU_BUTTON.classList.remove("hidden");
+  OVERLAY.classList.add("hidden");
 
   navOpen = false;
 }
 
-OVERLAY.addEventListener('click', closeNav);
+OVERLAY.addEventListener("click", closeNav);
 
 window.onscroll = function (e) {
   if (navOpen) {
@@ -36,12 +36,11 @@ window.onscroll = function (e) {
 
 function listHover(o) {
   return function (e) {
-    console.log(e.target);
-    if (e.target.classList.contains('hero__nav-link')) {
+    if (e.target.classList.contains("home__nav-link")) {
       const link = e.target;
       const siblings = link
-        .closest('.hero__nav')
-        .querySelectorAll('.hero__nav-link');
+        .closest(".home__nav")
+        .querySelectorAll(".home__nav-link");
 
       siblings.forEach((el) => {
         if (el !== link) el.style.opacity = o;
@@ -50,27 +49,27 @@ function listHover(o) {
   };
 }
 
-NAV.addEventListener('mouseover', listHover(0.5));
-NAV.addEventListener('mouseout', listHover(1));
+NAV.addEventListener("mouseover", listHover(0.5));
+NAV.addEventListener("mouseout", listHover(1));
 
 // TODO: Fix dis ↓↓↓
 function textSlide(entries) {
   const [entry] = entries;
   console.log(entry);
-  entry.target.classList.remove('reveal');
+  entry.target.classList.remove("reveal");
 }
 
 const aboutObserver = new IntersectionObserver(textSlide, {
-  root: document.querySelector('.content__text'),
+  root: document.querySelector(".content__text"),
   threshold: 1,
 });
 
-aboutObserver.observe(document.querySelector('.content__text'));
+aboutObserver.observe(document.querySelector(".content__text"));
 
 var rot = 360;
 
-SEAL.addEventListener('click', function () {
-  SEAL.style.transform = 'rotate(' + rot + 'deg)';
+SEAL.addEventListener("click", function () {
+  SEAL.style.transform = "rotate(" + rot + "deg)";
   rot += 360;
   console.log(rot);
   console.log(SEAL.style);
