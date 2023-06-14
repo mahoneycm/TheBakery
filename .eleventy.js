61eb1e763a0f07eaaea81fdc873e4c8e357dedce
Charlie <charlie.mahoneyy@gmail.com>
2023-06-13 23:00:13 -0400

    Create studio image collections for template

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,18 @@
+const fg = require("fast-glob");
+
+const studioAImages = fg.sync(["**/img/photos/studio-a/*", "!**/_site"]);
+const studioBImages = fg.sync(["**/img/photos/studio-b/*", "!**/_site"]);
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("index.js");
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addCollection("studio-a-img", function (collection) {
+    return studioAImages;
+  });
+  eleventyConfig.addCollection("studio-b-img", function (collection) {
+    return studioBImages;
+  });
   return {
     dir: {
       includes: "_includes",
